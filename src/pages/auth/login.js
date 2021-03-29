f6e621ae30bafebd53c33036771c3aeda9aa0e1d
lpeil <luan@peil.dev>
2021-03-29 20:26:05 -0300

    feat: redirect discord login

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Grid, Card, Button } from '@material-ui/core'
 
-const LoginPage = () => (
-  <Grid container justify="center" alignItems="center" style={{height: "100vh"}}>
-    <Card style={{ padding: "10px" }}>
-      <Button color="primary">Login with Discord</Button>
-    </Card>  
-  </Grid>
-)
+const LoginPage = () => {
+  const discordLogin = () => {
+    window.location.href = process.env.DISCORD_LOGIN;
+  }
+
+  return (
+    <Grid container justify="center" alignItems="center" style={{height: "100vh"}}>
+      <Card style={{ padding: "10px" }}>
+        <Button onClick={discordLogin} color="primary">Login with Discord</Button>
+      </Card>  
+    </Grid>
+  )
+}
 
 export default LoginPage;
